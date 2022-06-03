cf212032647a6c06a66481974aaa2c74d9e39951
Marius <marius@example.com>
2022-06-03 11:30:49 +0300

    counterList password field

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Counter from './components/Counter/Counter';
+import CounterList from './components/Counter/CounterList';
 import Login from './components/Form/Login';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className='App'>
       <h1>Heloo again world</h1>
       {/* TODO: add multile conters with titles */}
+      {/* <CounterList /> */}
       {/* <Counter />
       <Counter />
       <Counter /> */}
--- src/components/Form/Login.jsx
+++ src/components/Form/Login.jsx
@@ -3,18 +3,27 @@ import { useState } from 'react';
 
 function Login() {
   const [usernameValue, setUsernameValue] = useState('');
+  const [passwordValue, setPasswordValue] = useState('');
+
   const [showUsername, setShowUsername] = useState(false);
 
   /**
    * take input value from input and update state with it
    */
   function usernameEnterHandler(event) {
-    console.log('entering value to input');
-    console.log(event.target); // ===  <input type='text' placeholder='Login here' />
+    // console.log('entering value to input');
+    // console.log(event.target); // ===  <input type='text' placeholder='Login here' />
     const inputEl = event.target;
     const ivestaReikme = inputEl.value;
     setUsernameValue(ivestaReikme);
   }
+  function passwordEnterHandler(event) {
+    setPasswordValue(event.target.value);
+  }
+
+  // prideti slaptazodzio ivesties lauka
+  // sujungti slaptazodi su state two way binding
+  // isvalyti slaptazodi mygtuko paspaudimu
 
   return (
     <div className='login'>
@@ -24,6 +33,13 @@ function Login() {
         type='text'
         placeholder='Login here'
       />
+      <input
+        onChange={passwordEnterHandler}
+        value={passwordValue}
+        type='password'
+        placeholder='Password'
+      />
+
       <h3>Jus ivedete: {usernameValue}</h3>
 
       <button onClick={sendValues}>send</button>
@@ -33,8 +49,18 @@ function Login() {
 
   function sendValues() {
     // send values
+    // pagaminti objekta is input reiksmiu
+    // kai mums reikia reiksmiu mes imam is state
+    // nes sate yra sinkronizuota su inputais
+    const loginObj = {
+      username: usernameValue,
+      password: passwordValue,
+    };
+    console.log('loginObj ===', loginObj);
+
     // clear inputs
     setUsernameValue('');
+    setPasswordValue('');
   }
 
   function displayValue() {
