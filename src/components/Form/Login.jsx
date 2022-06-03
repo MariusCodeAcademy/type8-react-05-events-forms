2495a38efc1da8d6d189a822e38c430f97db8477
Marius <marius@example.com>
2022-06-03 11:02:50 +0300

    rwo way binding

--- src/components/Form/Login.jsx
+++ src/components/Form/Login.jsx
@@ -1,19 +1,46 @@
 // Login.jsx componentas
+import { useState } from 'react';
+
 function Login() {
-  // sukuri displayValue funkcija su test console.log
-  function displayValue() {
-    console.log('displayValue called');
+  const [usernameValue, setUsernameValue] = useState('');
+  const [showUsername, setShowUsername] = useState(false);
+
+  /**
+   * take input value from input and update state with it
+   */
+  function usernameEnterHandler(event) {
+    console.log('entering value to input');
+    console.log(event.target); // ===  <input type='text' placeholder='Login here' />
+    const inputEl = event.target;
+    const ivestaReikme = inputEl.value;
+    setUsernameValue(ivestaReikme);
   }
+
   return (
     <div className='login'>
-      {/* // vienas input type text */}
-      <input type='text' placeholder='Login here' />
-      {/* // vienas button */}
-      <button onClick={displayValue}>Submit</button>
+      <input
+        onChange={usernameEnterHandler}
+        value={usernameValue}
+        type='text'
+        placeholder='Login here'
+      />
+      <h3>Jus ivedete: {usernameValue}</h3>
+
+      <button onClick={sendValues}>send</button>
+      {showUsername && <h2>{usernameValue}</h2>}
     </div>
   );
+
+  function sendValues() {
+    // send values
+    // clear inputs
+    setUsernameValue('');
+  }
+
+  function displayValue() {
+    console.log('displayValue called');
+    setShowUsername(!showUsername);
+  }
 }
-// paspaudus button iskvieciam funkcija displayValue
-// susikurti state kuriame saugosim username
 
 export default Login;
