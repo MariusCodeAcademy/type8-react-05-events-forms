150103ee982fccf0607feaa02c728f2a70972e0a
Marius <marius@example.com>
2022-06-03 10:28:50 +0300

    Login b4 input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Counter from './components/Counter/Counter';
+import Login from './components/Form/Login';
 
 function App() {
   return (
     <div className='App'>
       <h1>Heloo again world</h1>
+      {/* TODO: add multile conters with titles */}
+      {/* <Counter />
       <Counter />
+      <Counter /> */}
+      <Login />
     </div>
   );
 }
--- src/components/Form/Login.jsx
+++ src/components/Form/Login.jsx
@@ -1,5 +1,19 @@
 // Login.jsx componentas
-// vienas input type text
-// vienas button
-// sukuri displayValue funkcija su test console.log
+function Login() {
+  // sukuri displayValue funkcija su test console.log
+  function displayValue() {
+    console.log('displayValue called');
+  }
+  return (
+    <div className='login'>
+      {/* // vienas input type text */}
+      <input type='text' placeholder='Login here' />
+      {/* // vienas button */}
+      <button onClick={displayValue}>Submit</button>
+    </div>
+  );
+}
 // paspaudus button iskvieciam funkcija displayValue
+// susikurti state kuriame saugosim username
+
+export default Login;
